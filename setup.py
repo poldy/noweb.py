da3b311eb5b5ae4d89657a6d2aca777e7c974ab9
dragontortoise <dragontortoise888@gmail.com>
2016-12-06 22:56:34 +0700

    noweb.py is a program which is designed to be executable from the command-line.  However, at first, I used "py_modules" option which treats 'noweb' as a module to be imported instead of a standalone script which can be called from the command-line.  I fix it by using "scripts" option instead.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
   name = 'noweb.py',
-  version = '1.0.2',
-  py_modules = ['noweb'],
+  version = '1.0.3',
+  scripts = ['noweb.py'],
   author = 'dragontortoise',
   author_email = 'dragontortoise888@gmail.com',
   url = 'https://github.com/dragontortoise/noweb.py',
